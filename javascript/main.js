3e8ff2debaaf1b03e0fe7d28ff22106f7946d965
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-05-12 23:09:52 +0200

    Added functionality to randomly place full formation fpor both players

--- javascript/main.js
+++ javascript/main.js
@@ -16,7 +16,6 @@ todo                 Javascript - what a wonderful language!
 
 //#region Open Jobs  
 /*
-todo Argument validation in every function? With passed argument types?
 todo Human placement function
 todo cpu placement thinking? dont allow placement as long as the cpu didnt placed (or make the cpu placement instantly....)
 todo display images for human placements. Idea: for 3 fields ship cut a image in 3 pieces and than place it.
@@ -26,6 +25,7 @@ todo Animations for attacking, hitted or sunken ships
 todo correct height and width of the gameboards
 todo text check
 todo UI styling
+todo Argument validation in every function? With passed argument types?
 todo every class and attribute needed?
 todo Function cutting and proof if needed
 todo CSS Minimizing and structuring
@@ -52,167 +52,167 @@ MainGameLoop = (playerName) => {
   const Human = new Player(playerName, true, player_Gameboard, cpu_Gameboard, info); // ? Create human player object
   const CPU = new Player(`CPU`, false, cpu_Gameboard, player_Gameboard, info); // ? Create cpu player object
 
-  player_Gameboard.placement("Submarine", [3, 5], [3, 7]); // ? Placing a Submarine on the gameboard in the 1 column from r ow 3 to 5
+  // player_Gameboard.placement("Submarine", [3, 5], [3, 7]); // ? Placing a Submarine on the gameboard in the 1 column from r ow 3 to 5
 
-  // ! placingRandomShipFormation() needs existing gameboards and players. It expliciy have to live here in the code!
-  placingRandomShipFormation = () => {     
-  
-  randomPlacement = (player, gameboard, shipType) => {   // ? Player must be a 'human' or 'cpu' string with 'Destroyer', 'Submarine', 'Cruiser', 'Battleship'or 'Carrier' ship type
-  // ? Argument validation
-  if(typeof player !== 'string') throw new TypeError('Only strings are allowed as player arguments.');
-  if(typeof shipType !== 'string') throw new TypeError(`The shipType argument must be a 'string'`);
-  
-  this.player = player;
-  this.shipType = shipType;
-  this.gameboard = gameboard;
-  sizeY = gameboard.sizeY;
-  sizeX = gameboard.sizeX;
-  
-  if(shipType === `Destroyer`){
-      coordinates = randomShipPlacementValues(`Destroyer`, sizeY, sizeX);  // console.log(destroyerCoordinates); // console.log([destroyerCoordinates.start[0],destroyerCoordinates.start[1]], [destroyerCoordinates.end[0], destroyerCoordinates.end[1]]);
-      if(typeof coordinates.start[0] !== 'number' || typeof coordinates.start[1] !== 'number' || typeof coordinates.end[0] !== 'number' || typeof coordinates.end[1] !== 'number'){
-          return false;
-      };
+  // ? Invoking full ship formation by invoking placingRandomShipFormation() with passing true as argument for human gameboard and false for cpu gameboard
+  //+++ placingRandomShipFormation() needs existing gameboards and players. It expliciy have to live here in the code  +++
+  placingRandomShipFormation = (human) => {     
 
-      freeField = proofFieldForFree(coordinates, CPU);
-      if(freeField === false){
-          return false;
-      };
+    // ? Argument Validation
+    if(typeof human !== 'boolean') throw new TypeError(`Argument human must be a 'boolean'. You have passed a ${typeof human}`);
+    let player, gameboard;
+    human === true ? player = `human` : player = `cpu`;
+    human === true ? gameboard = player_Gameboard : gameboard = cpu_Gameboard;
 
-      if(document.querySelector(`.${player}${shipType}`) !== null) return false; // ? Double placement security
+    randomPlacement = (player, gameboard, shipType) => {   // ? Player must be a 'human' or 'cpu' string with 'Destroyer', 'Submarine', 'Cruiser', 'Battleship'or 'Carrier' ship type
+    // ? Argument validation
+    if(typeof player !== 'string') throw new TypeError('Only strings are allowed as player arguments.');
+    if(typeof shipType !== 'string') throw new TypeError(`The shipType argument must be a 'string'`);
+    
+    this.player = player;
+    this.shipType = shipType;
+    this.gameboard = gameboard;
+    sizeY = gameboard.sizeY;
+    sizeX = gameboard.sizeX;
+    
+    if(shipType === `Destroyer`){
+        coordinates = randomShipPlacementValues(`Destroyer`, sizeY, sizeX);  // console.log(destroyerCoordinates); // console.log([destroyerCoordinates.start[0],destroyerCoordinates.start[1]], [destroyerCoordinates.end[0], destroyerCoordinates.end[1]]);
+        if(typeof coordinates.start[0] !== 'number' || typeof coordinates.start[1] !== 'number' || typeof coordinates.end[0] !== 'number' || typeof coordinates.end[1] !== 'number'){
+            return false;
+        };
 
-      if(player === `human`)  player_Gameboard.placement(`Destroyer`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
-      if(player === `cpu`)  cpu_Gameboard.placement(`Destroyer`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
-      console.log(`Placed ${shipType}`);
-      return true;
-  };
+        freeField = proofFieldForFree(coordinates, CPU);
+        if(freeField === false){
+            return false;
+        };
 
-  if(shipType === `Submarine`){
-      coordinates = randomShipPlacementValues(`Submarine`, sizeY, sizeX);  // console.log(destroyerCoordinates); // console.log([destroyerCoordinates.start[0],destroyerCoordinates.start[1]], [destroyerCoordinates.end[0], destroyerCoordinates.end[1]]);
-      if(typeof coordinates.start[0] !== 'number' || typeof coordinates.start[1] !== 'number' || typeof coordinates.end[0] !== 'number' || typeof coordinates.end[1] !== 'number'){
-          return false;
-      };
-  
-      freeField = proofFieldForFree(coordinates, CPU);
-      if(freeField === false){
-          return false;
-      };
+        if(document.querySelector(`.${player}${shipType}`) !== null) return false; // ? Double placement security
 
-      if(document.querySelector(`.${player}${shipType}`) !== null) return false; // ? Double placement security
+        if(player === `human`)  player_Gameboard.placement(`Destroyer`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
+        if(player === `cpu`)  cpu_Gameboard.placement(`Destroyer`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
+        return true;
+    };
 
-      if(player === `human`)  player_Gameboard.placement(`Submarine`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
-      if(player === `cpu`)  cpu_Gameboard.placement(`Submarine`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
-      console.log(`Placed ${shipType}`);
-      return true;
-  };
+    if(shipType === `Submarine`){
+        coordinates = randomShipPlacementValues(`Submarine`, sizeY, sizeX);  // console.log(destroyerCoordinates); // console.log([destroyerCoordinates.start[0],destroyerCoordinates.start[1]], [destroyerCoordinates.end[0], destroyerCoordinates.end[1]]);
+        if(typeof coordinates.start[0] !== 'number' || typeof coordinates.start[1] !== 'number' || typeof coordinates.end[0] !== 'number' || typeof coordinates.end[1] !== 'number'){
+            return false;
+        };
+    
+        freeField = proofFieldForFree(coordinates, CPU);
+        if(freeField === false){
+            return false;
+        };
 
-  if(shipType === `Cruiser`){
-      coordinates = randomShipPlacementValues(`Cruiser`,  sizeY, sizeX); 
-      if(typeof coordinates.start[0] !== 'number' || typeof coordinates.start[1] !== 'number' || typeof coordinates.end[0] !== 'number' || typeof coordinates.end[1] !== 'number'){
-          return false;
-      };
-      
-      freeField = proofFieldForFree(coordinates, CPU);
-      if(freeField === false){
-          return false;
-      };
+        if(document.querySelector(`.${player}${shipType}`) !== null) return false; // ? Double placement security
 
-      if(document.querySelector(`.${player}${shipType}`) !== null) return false; // ? Double placement security
+        if(player === `human`)  player_Gameboard.placement(`Submarine`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
+        if(player === `cpu`)  cpu_Gameboard.placement(`Submarine`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
+        return true;
+    };
 
-      if(player === `human`)  player_Gameboard.placement(`Cruiser`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
-      if(player === `cpu`)  cpu_Gameboard.placement(`Cruiser`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
-      console.log(`Placed ${shipType}`);
+    if(shipType === `Cruiser`){
+        coordinates = randomShipPlacementValues(`Cruiser`,  sizeY, sizeX); 
+        if(typeof coordinates.start[0] !== 'number' || typeof coordinates.start[1] !== 'number' || typeof coordinates.end[0] !== 'number' || typeof coordinates.end[1] !== 'number'){
+            return false;
+        };
+        
+        freeField = proofFieldForFree(coordinates, CPU);
+        if(freeField === false){
+            return false;
+        };
 
-      return true;
-  };
+        if(document.querySelector(`.${player}${shipType}`) !== null) return false; // ? Double placement security
 
-  if(shipType === `Battleship`){
-      coordinates = randomShipPlacementValues(`Battleship`, sizeY, sizeX);  // console.log(destroyerCoordinates); // console.log([destroyerCoordinates.start[0],destroyerCoordinates.start[1]], [destroyerCoordinates.end[0], destroyerCoordinates.end[1]]);
-      if(typeof coordinates.start[0] !== 'number' || typeof coordinates.start[1] !== 'number' || typeof coordinates.end[0] !== 'number' || typeof coordinates.end[1] !== 'number'){
-          return false;
-      };
-      
-      freeField = proofFieldForFree(coordinates, CPU);
-      if(freeField === false){
-          return false;
-      };
+        if(player === `human`)  player_Gameboard.placement(`Cruiser`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
+        if(player === `cpu`)  cpu_Gameboard.placement(`Cruiser`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
+        return true;
+    };
 
-      if(document.querySelector(`.${player}${shipType}`) !== null) return false; // ? Double placement security
+    if(shipType === `Battleship`){
+        coordinates = randomShipPlacementValues(`Battleship`, sizeY, sizeX);  // console.log(destroyerCoordinates); // console.log([destroyerCoordinates.start[0],destroyerCoordinates.start[1]], [destroyerCoordinates.end[0], destroyerCoordinates.end[1]]);
+        if(typeof coordinates.start[0] !== 'number' || typeof coordinates.start[1] !== 'number' || typeof coordinates.end[0] !== 'number' || typeof coordinates.end[1] !== 'number'){
+            return false;
+        };
+        
+        freeField = proofFieldForFree(coordinates, CPU);
+        if(freeField === false){
+            return false;
+        };
 
-      if(player === `human`)  player_Gameboard.placement(`Battleship`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
-      if(player === `cpu`)  cpu_Gameboard.placement(`Battleship`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
-      console.log(`Placed ${shipType}`);
-      return true;
-  };
+        if(document.querySelector(`.${player}${shipType}`) !== null) return false; // ? Double placement security
 
-  if(shipType === `Carrier`){
-      coordinates = randomShipPlacementValues(`Carrier`, sizeY, sizeX);  // console.log(destroyerCoordinates); // console.log([destroyerCoordinates.start[0],destroyerCoordinates.start[1]], [destroyerCoordinates.end[0], destroyerCoordinates.end[1]]);
-      if(typeof coordinates.start[0] !== 'number' || typeof coordinates.start[1] !== 'number' || typeof coordinates.end[0] !== 'number' || typeof coordinates.end[1] !== 'number'){
-          return false;
-      };
+        if(player === `human`)  player_Gameboard.placement(`Battleship`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
+        if(player === `cpu`)  cpu_Gameboard.placement(`Battleship`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
+        return true;
+    };
 
-      freeField = proofFieldForFree(coordinates, CPU);
-      if(freeField === false){
-          return false;
-      };
+    if(shipType === `Carrier`){
+        coordinates = randomShipPlacementValues(`Carrier`, sizeY, sizeX);  // console.log(destroyerCoordinates); // console.log([destroyerCoordinates.start[0],destroyerCoordinates.start[1]], [destroyerCoordinates.end[0], destroyerCoordinates.end[1]]);
+        if(typeof coordinates.start[0] !== 'number' || typeof coordinates.start[1] !== 'number' || typeof coordinates.end[0] !== 'number' || typeof coordinates.end[1] !== 'number'){
+            return false;
+        };
 
-      if(document.querySelector(`.${player}${shipType}`) !== null) return false; // ? Double placement security
+        freeField = proofFieldForFree(coordinates, CPU);
+        if(freeField === false){
+            return false;
+        };
 
-      if(player === `human`)  player_Gameboard.placement(`Carrier`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
-      if(player === `cpu`)  cpu_Gameboard.placement(`Carrier`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
-      console.log(`Placed ${shipType}`);
-      return true;
-  };
+        if(document.querySelector(`.${player}${shipType}`) !== null) return false; // ? Double placement security
 
-  throw new Error(`Only the strings 'Destroyer', 'Submarine', 'Cruiser', 'Battleship' or 'Carrier' are allowed as ship type.`) // ? If nothing returned before there must be a problem with the shipType string
-  };
+        if(player === `human`)  player_Gameboard.placement(`Carrier`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
+        if(player === `cpu`)  cpu_Gameboard.placement(`Carrier`, [coordinates.start[0], coordinates.start[1]], [coordinates.end[0], coordinates.end[1]]);  
+        return true;
+    };
 
-  destroyer = () =>{
-      val = randomPlacement(`cpu`, cpu_Gameboard,  `Destroyer`);
-      if(val !== true) {
-          destroyer();
-      };
-  }; 
+    throw new Error(`Only the strings 'Destroyer', 'Submarine', 'Cruiser', 'Battleship' or 'Carrier' are allowed as ship type.`) // ? If nothing returned before there must be a problem with the shipType string
+    };
 
-  submarine = () =>{
-      sub = randomPlacement(`cpu`, cpu_Gameboard,  `Submarine`);
-      if(sub !== true) {
-          submarine();
-      };
-  };
+    destroyer = () =>{
+        val = randomPlacement(player, gameboard,  `Destroyer`);
+        if(val !== true) {
+            destroyer();
+        };
+    }; 
 
-  cruiser = () => {
-      val = randomPlacement(`cpu`, cpu_Gameboard, `Cruiser`);
-      if(val !== true) {
-          cruiser();
-      };
-  };
+    submarine = () =>{
+        val = randomPlacement(player, gameboard,  `Submarine`);
+        if(val !== true) {
+            submarine();
+        };
+    };
 
-  battleship = () => {
-      val = randomPlacement(`cpu`, cpu_Gameboard, `Battleship`);
-      if(val !== true) {
-          battleship();
-      };
-  };
+    cruiser = () => {
+        val = randomPlacement(player, gameboard, `Cruiser`);
+        if(val !== true) {
+            cruiser();
+        };
+    };
 
-  carrier = () => {
-      val = randomPlacement(`cpu`, cpu_Gameboard, `Carrier`);
-      if(val !== true)  {
-          carrier();
-      };
-  };
+    battleship = () => {
+        val = randomPlacement(player, gameboard, `Battleship`);
+        if(val !== true) {
+            battleship();
+        };
+    };
 
-  destroyer();
-  submarine();
-  cruiser();
-  battleship();
-  carrier();
+    carrier = () => {
+        val = randomPlacement(player, gameboard, `Carrier`);
+        if(val !== true)  {
+            carrier();
+        };
+    };
 
-  return {destroyer, submarine, cruiser, battleship, carrier};
+    // ? Invoke ship creation and random placement
+    destroyer(); submarine(); cruiser(); battleship(); carrier();
+
+    return true;
 
   };
-  // ? Invoke ship placement for cpu
-  placingRandomShipFormation();
+
+  // ? Invoke ship placement
+  placingRandomShipFormation(true);
+  placingRandomShipFormation(false);
 
   // ? Trigger cpu attack after a human attack. Check every interVal if CPU is on turn
   interVal = 100;
